17f6c46f8fe4a7c572132636c26654e4ea608fd9
saransh-209 <s2009.saransh@gmail.com>
2026-05-10 18:48:21 +0530

    fixed expresss

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+const router = require('./router');
 
 const User = require("./models/User");
 const Post = require("./models/Post");
@@ -16,7 +17,7 @@ const app = express();
 
 /* MIDDLEWARE */
 app.use(cors());
-
+app.use(router());
 app.use(express.json({
 limit:"10mb"
 }));
