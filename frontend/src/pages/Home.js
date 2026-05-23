8825446b648919ea8707f93ee09e6c84f3d6366f
saransh-209 <s2009.saransh@gmail.com>
2026-05-23 18:52:44 +0530

    mobile responsive home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "../index.css";
+import mobileBg from "../assets/mobile-bg.jpg";
 
 function Home(){
 
@@ -44,6 +45,7 @@ setShowRetry(true);
 setLoading(false);
 return;
 }
+
 setLoading(true);
 setShowRetry(false);
 
@@ -118,22 +120,25 @@ setShowLogoutModal(false);
 return(
 <div style={token ? styles.container : styles.fullBgContainer}>
 <div style={{flex:1}}>  
+
 <div style={styles.navbar(theme)}>
-<h2 style={styles.logo(theme)}>परवा पाइख</h2>
+
+<h2 style={styles.logo(theme)}>
+परवा पाइख
+</h2>
 
 <div style={styles.navRight}>
 
 <button onClick={toggleTheme} style={styles.themeBtn(theme)}>
-  {theme === "dark" ? "🌙" : "☀️"}
+{theme === "dark" ? "🌙" : "☀️"}
 </button>
 
 {token ? (
 <>
 {role==="author" && (
-<button onClick={() => navigate("/create")}
+<button
+onClick={() => navigate("/create")}
 style={styles.createBtn(theme)}
-onMouseEnter={(e)=>e.target.style.background='rgba(255,255,255,0.1)'}
-onMouseLeave={(e)=>e.target.style.background='transparent'}
 >
 + Create
 </button>
@@ -153,11 +158,17 @@ Logout
 </>
 ):(
 <>
-<button style={styles.loginBtn(theme)} onClick={()=>navigate('/login')}>
+<button
+style={styles.loginBtn(theme)}
+onClick={()=>navigate('/login')}
+>
 Login
 </button>
 
-<button style={styles.signupBtn(theme)} onClick={()=>navigate('/signup')}>
+<button
+style={styles.signupBtn(theme)}
+onClick={()=>navigate('/signup')}
+>
 Sign Up
 </button>
 </>
@@ -170,10 +181,13 @@ Sign Up
 {!token && (
 <>
 <div style={styles.centerBox}>
+
 <div className="glass-box" style={styles.glass}>
 
 <h1 style={styles.heading(theme)}>
-Discover <span style={styles.highlight(theme)}>मैथिली साहित्य</span>
+Discover <span style={styles.highlight(theme)}>
+मैथिली साहित्य
+</span>
 </h1>
 
 <p style={styles.subtext(theme)}>
@@ -190,27 +204,42 @@ Get Started →
 </div>
 </div>
 
+
 <section style={styles.whySection}>
-<h2 style={styles.whyTitle}>Why Use This Platform?</h2>
+
+<h2 style={styles.whyTitle}>
+Why Use This Platform?
+</h2>
 
 <div style={styles.whyGrid}>
+
 <div style={styles.whyCard}>
 ✍️
 <h3>Create Content</h3>
-<p style={styles.whytext}>Empower creators to write, edit, and publish original posts seamlessly through an intuitive content creation experience.</p>
+
+<p style={styles.whytext}>
+Empower creators to write, edit, and publish original posts seamlessly through an intuitive content creation experience.
+</p>
 </div>
 
 <div style={styles.whyCard}>
 📖
 <h3>Read & Explore</h3>
-<p style={styles.whytext}>Explore a diverse collection of poetry, stories, and articles shared by talented authors worldwide.</p>
+
+<p style={styles.whytext}>
+Explore a diverse collection of poetry, stories, and articles shared by talented authors worldwide.
+</p>
 </div>
 
 <div style={styles.whyCard}>
 ⚡
 <h3>Fast & Simple</h3>
-<p style={styles.whytext}>Experience a fast, responsive, and modern interface designed for seamless navigation and user comfort.</p>
+
+<p style={styles.whytext}>
+Experience a fast, responsive, and modern interface designed for seamless navigation and user comfort.
+</p>
 </div>
+
 </div>
 </section>
 </>
@@ -220,13 +249,20 @@ Get Started →
 {token && (
 <>
 <section style={styles.heroSmall}>
+
 <h1 style={styles.headingLight(theme)}>
-Discover <span style={styles.highlight(theme)}>मैथिली साहित्य</span>
+Discover <span style={styles.highlight(theme)}>
+मैथिली साहित्य
+</span>
 </h1>
+
 </section>
 
 <div style={styles.postsSection(theme)}>
-<h2 style={styles.sectionTitle(theme)}>Latest Posts</h2>
+
+<h2 style={styles.sectionTitle(theme)}>
+Latest Posts
+</h2>
 
 <div style={styles.searchWrap}>
 
@@ -337,6 +373,7 @@ No Cover
 </div>
 
 <div style={styles.cardBody(theme)}>
+
 <h3 style={styles.postTitle(theme)}>
 {post.title}
 </h3>
@@ -344,6 +381,7 @@ No Cover
 <p style={styles.author(theme)}>
 ✍ {post.author}
 </p>
+
 </div>
 
 </div>
@@ -354,42 +392,16 @@ No Cover
 
 </div>
 
-
-{showRetry && loading && (
-<div style={styles.retryOverlay}>
-
-<div style={styles.retryModal}>
-
-<h2 style={styles.retryTitle}>
-Network Issue ⚠️
-</h2>
-
-<p style={styles.retryText}>
-Posts are taking longer to load.
-Waiting for internet reconnection...
-</p>
-
-<button
-onClick={fetchPosts}
-style={styles.retryBtn}
->
-Try Again
-</button>
-
-</div>
-
-</div>
-)}
-
-
 {visiblePosts < filteredPosts.length && !loading && (
 <div style={{textAlign:'center',marginTop:'20px',marginBottom:'20px'}}>
+
 <button
 style={styles.postBtn(theme)}
 onClick={()=>setVisiblePosts(visiblePosts+12)}
 >
 See More Posts ↓
 </button>
+
 </div>
 )}
 
@@ -397,9 +409,9 @@ See More Posts ↓
 </>
 )}
 
-
 {showLogoutModal && (
 <div style={styles.overlay}>
+
 <div style={styles.modal}>
 
 <h2 style={{marginBottom:'10px'}}>
@@ -438,6 +450,7 @@ Logout
 )}
 
 </div>
+
 <footer style={styles.footer(theme)}>
 <p>© copyright 2026 Saransh | All Rights Reserved</p>
 </footer>
@@ -447,6 +460,7 @@ Logout
 }
 
 const styles={
+
 container:{
 background:'#1e1b4b',
 minHeight:'100vh',
@@ -455,29 +469,41 @@ flexDirection:'column'
 },
 
 fullBgContainer:{
-  height:'100%',
-  display:'flex',
-  flexDirection:'column',
-  justifyContent:'flex-start',
-  width:'100%',
-  backgroundImage:"url('https://res.cloudinary.com/djhio7kqd/image/upload/v1777919102/ChatGPT_Image_May_4_2026_11_47_57_PM_rsfpn5.png')",
-  backgroundSize:'100% auto',         
-  backgroundRepeat:'no-repeat',
-  backgroundPosition:'top center',
-  overflowX:'hidden',
-  backgroundColor:'rgb(17, 20, 54)'          
+minHeight:'100vh',
+display:'flex',
+flexDirection:'column',
+justifyContent:'flex-start',
+width:'100%',
+overflowX:'hidden',
+backgroundColor:'rgb(17, 20, 54)',
+
+backgroundImage:`
+linear-gradient(rgba(0,0,0,0.15), rgba(0,0,0,0.15)),
+url(${window.innerWidth <= 768
+? mobileBg
+: "https://res.cloudinary.com/djhio7kqd/image/upload/v1777919102/ChatGPT_Image_May_4_2026_11_47_57_PM_rsfpn5.png"})
+`,
+
+backgroundSize:
+window.innerWidth <= 768
+? 'cover'
+: '100% auto',
+
+backgroundRepeat:'no-repeat',
+
+backgroundPosition:
+window.innerWidth <= 768
+? 'center top'
+: 'top center',
 },
 
 themeBtn:(theme)=>({
 padding:'8px 12px',
 borderRadius:'8px',
-background:
- theme === "dark"? "linear-gradient(135deg, #0d0d0d, #6b6b71)": "linear-gradient(135deg, #0d0d0d, #6b6b71)",
-color: theme === "dark" ? "#fff" : "#000",
-border: `1px solid ${theme==="dark" ? '#fff' : '#111'}`,
-cursor:'pointer',
-transition:'0.3s',
-boxShadow:'0 0 0 transparent',
+background:"linear-gradient(135deg, #0d0d0d, #6b6b71)",
+color:"#fff",
+border:`1px solid #fff`,
+cursor:'pointer'
 }),
 
 navbar:(theme)=>({
@@ -493,21 +519,13 @@ background:
 theme==="dark"
 ? 'rgba(15, 23, 42, 0.6)'
 : 'rgba(226, 226, 207, 0.83)',
-backdropFilter:'blur(0px)',
-WebkitBackdropFilter:'blur(18px)',
-borderBottom:
-theme==="dark"
-? '1px solid rgba(255,255,255,0.08)'
-: '1px solid rgba(0,0,0,0.06)',
-boxShadow:
-theme==="dark"? '0 4px 20px rgba(0,0,0,0.4)': '0 4px 20px rgba(0,0,0,0.08)',
-transition:'all 0.3s ease'
+backdropFilter:'blur(18px)',
+borderBottom:'1px solid rgba(255,255,255,0.08)',
 }),
 
 logo:(theme)=>({
 fontSize:'20px',
 fontWeight:'600',
-letterSpacing:'0.5px',
 color: theme==="dark" ? '#fff' : '#111',
 }),
 
@@ -518,17 +536,10 @@ alignItems:'center',
 marginRight:'15px'
 },
 
-iconBtn:{
-padding:'8px 10px',
-borderRadius:'8px',
-border:'1px solid rgba(64, 65, 68, 0.87)'
-},
-
 loginBtn:(theme)=>({
 padding:'8px 16px',
 background:theme==="dark" ? '#fff' : '#111',
 color:theme==="dark" ? '#111' : '#fff',
-border:`1px solid ${theme==="dark" ? '#111' : '#fff'}`,
 borderRadius:'8px'
 }),
 
@@ -536,80 +547,80 @@ signupBtn:(theme)=>({
 padding:'8px 16px',
 background:theme==="dark" ? '#111' : '#fff',
 color:theme==="dark" ? '#fff' : '#111',
-border:`1px solid ${theme==="dark" ? '#fff' : '#111'}`,
 borderRadius:'8px'
 }),
 
 createBtn:(theme)=>({
 padding:'8px 10px',
 borderRadius:'8px',
-border: `1px solid ${theme==="dark" ? '#fff' : '#111'}`,
+border:'1px solid #fff',
 background:'transparent',
-color: theme==="dark" ? '#fff' : '#111',
+color:'#fff',
 cursor:'pointer',
-transition:'all 0.25s ease',
 }),
 
 logoutBtn:{
 padding:'8px 14px',
 borderRadius:'10px',
-border: '1px solid #111',
 background:'linear-gradient(135deg,#ef4444,#dc2626)',
 color:'white',
-cursor:'pointer',
-transition:'all 0.25s ease',
 },
 
-welcome:(theme)=>({color:
-    theme==="dark" ? "#c2c4eb": "#161720",
+welcome:(theme)=>({
+color: theme==="dark" ? "#c2c4eb": "#161720",
 }),
 
 centerBox:{
-minHeight:'95vh',
+minHeight: window.innerWidth <= 768 ? '75vh' : '95vh',
 display:'flex',
 justifyContent:'center',
-alignItems:'center'
+alignItems:'center',
+padding:'20px'
 },
 
 glass:{
-padding:'50px',
+padding: window.innerWidth <= 768 ? '28px' : '50px',
+width: window.innerWidth <= 768 ? '90%' : 'auto',
 display:'flex',
 flexDirection:'column',
-alignItems:'center',   
-textAlign:'center'     
+alignItems:'center',
+textAlign:'center',
+borderRadius:'24px',
+backdropFilter:'blur(10px)',
+background:'rgba(0,0,0,0.18)'
 },
 
-heroSmall:{
-width:'100%',
-height:'45vh',   
-backgroundImage:"url('https://res.cloudinary.com/djhio7kqd/image/upload/v1777999093/ChatGPT_Image_May_5_2026_10_07_28_PM_cwmjde.png')",
-backgroundSize:'cover',           
-backgroundPosition:'center',   
-backgroundRepeat:'no-repeat',
-display:'flex',
-justifyContent:'center',
-alignItems:'center'
-},
+heading:(theme)=>({
+fontSize: window.innerWidth <= 768 ? '34px' : '45px',
+marginBottom:'10px',
+textAlign:'center',
+color:theme==="dark"
+? "#070e44da"
+: "#0986e5f4",
+}),
 
-heading:(theme)=>({fontSize:'45px', marginBottom:'10px',
-    color:theme==="dark" ? "#070e44da": "#0986e5f4",}
-  ),
-
-headingLight:(theme)=>({fontSize:'45px',
-    color:theme==="dark" 
+headingLight:(theme)=>({
+fontSize:'45px',
+color:theme==="dark"
 ? "#1850ebc6"
-: "#193063d4",}
-),
+: "#193063d4",
+}),
 
-highlight:(theme)=>({color:theme==="dark" 
+highlight:(theme)=>({
+color:theme==="dark"
 ? "#bc19a4d7"
-: "#4d2ab9e4",}
-),
+: "#4d2ab9e4",
+}),
 
-subtext:(theme)=>({marginTop:'0px', marginBottom:'20px', color:theme==="dark" 
+subtext:(theme)=>({
+marginTop:'0px',
+marginBottom:'20px',
+color:theme==="dark"
 ? "#0e0e0ebd"
 : "#6a401ee2",
-fontSize:'18px'}),
+fontSize: window.innerWidth <= 768 ? '15px' : '18px',
+textAlign:'center'
+}),
 
 primaryBtn:{
 padding:'12px 25px',
@@ -621,16 +632,13 @@ borderRadius:'8px'
 
 whySection:{
 width:'100%',
-padding:'100px 60px',
+padding:'100px 30px',
 background:'#020617',
-minHeight:'70vh',  
-paddingBottom:'100px'
 },
 
 whyTitle:{
 textAlign:'center',
 fontSize:'32px',
-marginTop:'1px',
 marginBottom:'80px',
 color:'white'
 },
@@ -640,7 +648,6 @@ display:'grid',
 gridTemplateColumns:'repeat(auto-fit,minmax(250px,1fr))',
 gap:'30px',
 maxWidth:'1300px',
-minHeight:'40vh',
 margin:'0 auto'
 },
 
@@ -650,159 +657,21 @@ padding:'40px',
 borderRadius:'24px',
 fontSize:'24px',
 color:'#a0e1ea',
-boxShadow:'0 15px 40px rgba(99,102,241,.18)',
-border:'1px solid rgba(255,255,255,.05)',
-transform:'translateY(-8px)'
 },
 
 whytext:{
-  marginTop:'15px',
-  fontSize:'18px',
-  color:'#e793e4',
-lineHeight:'1.5',
-},
-
-postsSection:(theme)=>({
-padding:'50px',
-background: theme === "dark" ? "#212031" : "#c3d5d8", 
-minHeight:'100vh',
-transition:'0.3s'
-}),
-
-sectionTitle:(theme)=>({
-fontSize:'40px',
-marginBottom:'30px',
-color:theme==="dark" 
-? "#16b4f3dc"
-: "#161515f3"
-}),
-
-grid:{
-display:'grid',
-gridTemplateColumns:'repeat(auto-fill,minmax(280px,1fr))',
-gap:'25px'
-},
-
-card:(theme)=>({
-background:theme==="dark" 
-? "#258cedbd"
-: "#3b1d95dc",
-borderRadius:'12px',
-overflow:'hidden',
-}),
-
-image:{
-height:'220px',
-overflow:'hidden',
-background:'#111827',
-position:'relative'
-},
-
-postImage:{
-width:'100%',
-height:'100%',
-objectFit:'cover',
-transition:'transform .6s ease',
-filter:'brightness(.92)',
-},
-
-noImage:{
-width:'100%',
-height:'100%',
-display:'flex',
-justifyContent:'center',
-alignItems:'center',
-background:'#1f2937',
-color:'#c2cad7'
-},
-
-cardBody:(theme)=>({padding:'15px',
-    color:theme==="dark" 
-? "#94ce0cbd"
-: "#0a9bc497",
-}),
-
-postTitle:(theme)=>({fontSize:'20px',marginBottom:'1px',
-    color:theme==="dark" 
-? "#ffffffec"
-: "#f5f8f9e5",}),
-
-author:(theme)=>({fontSize:'16px',marginTop:'10px', marginBottom:'6px',
-    color:
-    theme==="dark" 
-? "#e8738e"
-: "#efc08ed8"}),
-
-postBtn:(theme)=>({
-padding:'11px 18px',
+marginTop:'15px',
 fontSize:'18px',
-background:theme==="dark" 
-? "#258cedbd"
-: "#473cc1d9",
-color:'white',
-border:'2px solid #757376',
-borderRadius:'15px',
-fontWeight:'540'
-}),
-
-retryBtn:{
-padding:"14px 28px",
-background:"linear-gradient(135deg, #3a46ed, #332bc9)",
-color:"white",
-fontSize:"16px",
-border:"none",
-borderRadius:"14px",
-fontWeight:"560",
-cursor:"pointer"
-},
-
-retryOverlay:{
-position:"fixed",
-inset:0,
-display:"flex",
-justifyContent:"center",
-alignItems:"center",
-background:"rgba(0,0,0,0.35)",
-backdropFilter:"blur(8px)",
-zIndex:5000
-},
-
-retryModal:{
-width:"380px",
-padding:"35px",
-borderRadius:"24px",
-textAlign:"center",
-background:"rgba(255,255,255,0.08)",
-backdropFilter:"blur(18px)",
-border:"1px solid rgba(255,255,255,0.12)",
-boxShadow:"0 0 30px rgba(0,0,0,.35)"
-},
-
-retryTitle:{
-color:"white",
-fontSize:"28px",
-marginBottom:"12px"
-},
-
-retryText:{
-color:"#d6e7fe",
-lineHeight:"1.6",
-marginBottom:"25px"
+color:'#e793e4',
+lineHeight:'1.5',
 },
 
 footer:(theme)=>({
 padding:'20px',
 textAlign:'center',
-background:
-theme==="dark" 
-? "#08090bde"
-: "#c8d2f0ea",
-color:theme==="dark" 
-? "#fbfbfcbd"
-: "#0d0d0de8",
+background:'#08090bde',
+color:'#fbfbfcbd',
 marginTop:'auto',
-fontWeight:'450',
-marginBottom:'0',
 }),
 
 overlay:{
@@ -821,10 +690,7 @@ backdropFilter:'blur(18px)',
 padding:'35px',
 borderRadius:'22px',
 width:'400px',
-height:'fit-content',
 textAlign:'center',
-border:'1px solid rgba(255,255,255,.12)',
-boxShadow:'0 0 10px rgba(192, 18, 18, 0.45)',
 color:'white'
 },
 
@@ -846,118 +712,7 @@ yesBtn:{
 background:'linear-gradient(135deg,#ef4444,#dc2626)',
 color:'white',
 padding:'10px 18px',
-border:'none',
 borderRadius:'10px'
-},
-
-skeletonCard:{
-background:'#1f2937',
-borderRadius:'12px',
-overflow:'hidden',
-position:'relative'
-},
-
-skeletonShimmer:{
-position:'absolute',
-top:0,
-left:'-150%',
-width:'60%',
-height:'100%',
-background:'linear-gradient(90deg,transparent,rgba(255,255,255,.12),transparent)',
-animation:'shimmer 1.8s infinite'
-},
-
-skeletonImage:{
-height:'160px',
-background:'#374151'
-},
-
-skeletonLine:{
-height:'22px',
-width:'70%',
-margin:'20px',
-borderRadius:'8px',
-background:'#4b5563'
-},
-
-skeletonSmall:{
-height:'14px',
-width:'45%',
-margin:'20px',
-marginTop:'-5px',
-borderRadius:'8px',
-background:'#6b7280'
-},
-searchWrap:{
-position:"relative",
-display:"flex",
-gap:"12px",
-marginBottom:"35px",
-maxWidth:"400px"
-},
-
-searchInput:(theme)=>({
-flex:1,
-padding:"15px 18px",
-borderRadius:"14px",
-border:"1px solid rgba(255,255,255,.15)",
-background: theme === "dark" ? "#2e2a5a" : "#f3f4f6",
-color: theme === "dark" ? "#fff" : "#000",
-}),
-
-searchBtn:(theme)=>({
-padding:"14px 22px",
-border:"none",
-borderRadius:"14px",
-background: theme === "dark"
-  ? "linear-gradient(135deg, #6d28d9, #4f46e5)"
-  : "linear-gradient(180deg, #3d28d9, #46a8e5)",
-
-color: theme === "dark" ? "#fff" : "#fefafa",
-fontWeight:'300',
-fontSize:'16px'
-}),
-
-historyBox:{
-position:"absolute",
-top:"60px",
-left:0,
-width:"100%",
-background:"#111827",
-borderRadius:"16px",
-padding:"14px",
-zIndex:2000
-},
-
-historyTop:{
-display:"flex",
-justifyContent:"space-between",
-marginBottom:"12px",
-color:"#cbd5e1"
-},
-
-clearX:{
-cursor:"pointer",
-color:"#f54f4f"
-},
-
-historyItem:{
-padding:"10px",
-borderRadius:"10px",
-marginBottom:"6px",
-background:"rgba(255,255,255,.04)",
-color:"white",
-cursor:"pointer"
-},
-
-noPostBox:{
-gridColumn:"1 / -1",
-textAlign:"center",
-padding:"70px 20px",
-borderRadius:"20px",
-background:"rgba(255,255,255,.05)",
-backdropFilter:"blur(12px)",
-color:"white"
 },
 
 };
